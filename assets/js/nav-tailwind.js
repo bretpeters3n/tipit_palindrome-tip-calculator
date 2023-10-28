f583a150a96384e32046d3b80f6550b3e7bfafb9
Bret Petersen <bretpetersen82@gmail.com>
2023-10-28 18:19:07 -0500

    add new tailwind navigation bar. interactivity still not working. this is because the Play CDN doesn't include it. dont add menu until later

--- assets/js/nav-tailwind.js
+++ assets/js/nav-tailwind.js
@@ -1,8 +1,21 @@
-// Initialization for ES Users
-import {
-    Collapse,
-    Dropdown,
-    initTE,
-  } from "tw-elements";
-  
-  initTE({ Collapse, Dropdown });
+function navToggle() {
+    var btn = document.getElementById('menuBtn');
+    var nav = document.getElementById('menu');
+
+    btn.classList.toggle('open');
+    nav.classList.toggle('flex');
+    nav.classList.toggle('hidden');
+}
+
+var nav = document.getElementById('site-menu');
+var header = document.getElementById('top');
+
+window.addEventListener('scroll', function() {
+if (window.scrollY >=400) { // adjust this value based on site structure and header image height
+    nav.classList.add('nav-sticky');
+    header.classList.add('pt-scroll');
+} else {
+    nav.classList.remove('nav-sticky');
+    header.classList.remove('pt-scroll');
+}
+});
