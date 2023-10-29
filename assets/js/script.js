387d510afcab4bf56df2c2b5558a29417737b7dd
Bret Petersen <bretpetersen82@gmail.com>
2023-10-28 23:36:48 -0500

    Add ability to save tips in localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,6 @@ function navigate(url) {
     window.location.href=url;
 }
 
-
 // tip choice acions
 function tipChoice(choice) {
     if (choice=='high') {
@@ -24,6 +23,17 @@ function tipChoice(choice) {
         let tip = document.getElementById('hTip').innerText;
         let tipPerc = document.getElementById('hTipPerc').innerText;
         console.log(highLow + ' ' + total + ' ' + tip + ' ' + tipPerc);
+
+        // set and save tips object
+        let tipEvent = {
+            highLow: highLow,
+            total: total,
+            tip: tip,
+            tipPerc: tipPerc,
+        };
+        let tipEventTest = [tipEvent.highLow, tipEvent.total];
+        tipEvents.unshift(tipEventTest);
+        localStorage.setItem('tipEvent', JSON.stringify(tipEvents));
     } else if (choice=='low') {
         // console.log('You chose the low tip');
         let highLow = 'low';
@@ -31,7 +41,38 @@ function tipChoice(choice) {
         let tip = document.getElementById('lTip').innerText;
         let tipPerc = document.getElementById('lTipPerc').innerText;
         console.log(highLow + ' ' + total + ' ' + tip + ' ' + tipPerc);
+
+        // set and save tips object
+        let tipEvent = {
+            highLow: highLow,
+            total: total,
+            tip: tip,
+            tipPerc: tipPerc,
+        };
+        let tipEventTest = [tipEvent.highLow, tipEvent.total];
+        tipEvents.unshift(tipEventTest);
+        localStorage.setItem('tipEvent', JSON.stringify(tipEvents));
     } else {
         console.log('high or low not caught, error.');
     }
 }
+
+// saved tips object
+let tipEvent = {
+    highLow: 'high',
+    total: '$83.38',
+    tip: '$14.38',
+    tipPerc: '20.84%',
+};
+
+// initiate tipEvents array and add first object ^
+let tipEventTest = [tipEvent.highLow, tipEvent.total];
+let tipEvents = [];
+tipEvents.unshift(tipEventTest);
+
+// save in localStorage
+localStorage.setItem('tipEvent', JSON.stringify(tipEvents));
+
+
+// display tipEvents data on history page
+let history = document.getElementById('lTotal');
