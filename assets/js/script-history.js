4cbd0404c6a546aced95500572f6f2cc7bafb61e
Bret Petersen <bretpetersen82@gmail.com>
2023-10-29 22:18:24 -0500

    Add ability to erase tip history visually and through localStorage simultaneously

--- assets/js/script-history.js
+++ assets/js/script-history.js
@@ -20,7 +20,7 @@ tipEvents.forEach(function (tipEvent, index, tipEvents) {
 
     // add delete button to remove shopping list item
     tipEventItemEl.append(
-        '<button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full bg-white border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Remove</button>'
+        '<button id=' + index + ' class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full bg-white border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2 delete-item-btn" data-index-number=' + index + '>Remove</button>'
     );
 
     // print to the page
@@ -28,9 +28,25 @@ tipEvents.forEach(function (tipEvent, index, tipEvents) {
 
 });
 
-
+function handleRemoveItem(event) {
+    // convert button we pressed (`event.target`) to a jQuery DOM object
+    let btnClicked = $(event.target);
   
+    // get the parent `<div>` element from the button we pressed and remove it
+    btnClicked.parent('div').remove();
 
+    // log out index
+    let choiceIndex = event.target.id;
+
+    // remove the selected index from the array
+    tipEvents = tipEvents.slice(0, choiceIndex).concat(tipEvents.slice(choiceIndex + 1));
+
+    // save in localStorage
+    localStorage.setItem('tipEvents', JSON.stringify(tipEvents));
+}
+
+// use event delegation on the `shoppingListEl` to listen for click on any element with a class of `delete-item-btn`
+history.on('click', '.delete-item-btn', handleRemoveItem);
 
 // nav links
 function navigate(url) {
